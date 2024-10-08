a6585a29130b8691cc4b34f7e377af72e1efe9a5
Pavlin <pavling.georgiev@gmail.com>
2024-10-08 00:25:33 -0400

    added ast_test. Tests String() method

---
+++ tests/ast_test.go
@@ -0,0 +1,31 @@
+package tests
+
+import (
+	"interpreter/ast"
+	"interpreter/token"
+	"testing"
+)
+
+// Tests the String()
+func TestSring(t *testing.T) {
+	program := *&ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf(" program.String() Got : %q", program.String())
+	}
+
+}
